91296851c96da1548ce35d890a1259ec1173994b
Tom Rogers <tomrogers3@gmail.com>
2023-11-10 08:55:57 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify, send_from_directory, redirect, session
+from waitress import serve
+from tracks import EDUCATIONAL_TRACKS
+
+app = Flask(__name__)
+
+@app.route('/start_track', methods=['POST'])
+def start_track():
+  track_name = request.json.get('track')
+  EDUCATIONAL_TRACKS.keys()
+  track = EDUCATIONAL_TRACKS.get(track_name)
+  if track is None:
+    return jsonify({"error": "Track not found"}), 400
+  # Start the student at the first step of the track.
+  step_index = 0
+  step = track['steps'][step_index]
+  return jsonify({"prompt": step['prompt'], "step_index": step_index})
+
+@app.route('/next_step', methods=['POST'])
+def next_step():
+  track_name = request.json.get('track')
+  step_index = request.json.get('step_index')
+  user_response = request.json.get('user_response')
+
+  track = EDUCATIONAL_TRACKS.get(track_name)
+  if track is None:
+    return jsonify({"error": "Track not found"}), 400
+
+  if step_index >= len(track['steps']):
+    return jsonify({"error": "No more steps in this track"}), 400
+
+  # Provide feedback for the current step.
+  current_step = track['steps'][step_index]
+  feedback = current_step['response']
+
+  # Move to the next step if available.
+  step_index += 1
+  if step_index < len(track['steps']):
+    next_step = track['steps'][step_index]
+    return jsonify({
+      "feedback": feedback,  # Include feedback in the response
+      "next_prompt": next_step['prompt'],
+      "step_index": step_index
+    })
+  else:
+    return jsonify({
+      "feedback": feedback,  # Include feedback in the response
+      "next_prompt": None,
+      "step_index": step_index
+    })
+  
+@app.route('/.well-known/ai-plugin.json')
+def serve_ai_plugin():
+  return send_from_directory('.well-known',
+                             'ai-plugin.json',
+                             mimetype='application/json')
+
+
+@app.route('/openapi.yaml')
+def serve_openapi_yaml():
+  return send_from_directory('.', 'openapi.yaml', mimetype='text/yaml')
+
+
+@app.route('/logo.png')
+def serve_logo():
+  return send_from_directory('.', 'logo.png', mimetype='image/png')
+
+@app.route('/legal-info')
+def serve_legal_info():
+  return send_from_directory('.', 'legal-info', mimetype='text/yaml')
+
+
+if __name__ == '__main__':
+  serve(app, host="0.0.0.0", port=8080)
---
+++ tracks.py
@@ -0,0 +1,22 @@
+EDUCATIONAL_TRACKS = {
+  "prompt_engineering": {
+    "steps": [
+      # Section 1: Introduction to LLMs & Generative AI
+      {
+        "prompt":
+        "What is a Large Language Model (LLM)?",
+        "response":
+        "A Large Language Model (LLM) is a type of AI model that is trained on vast amounts of text data to understand and generate human-like text. LLMs are capable of various natural language processing tasks, such as language translation, text generation, question-answering, and more. GPT-3, developed by OpenAI, is an example of an LLM. You can learn more about LLMs here: https://www.mlq.ai/what-is-a-large-language-model-llm/"
+      },
+      # ... (include all steps from Section 1)
+      
+      # Section 2: Introduction to Prompt Engineering
+      # ... (include all steps from Section 2)
+      
+      #...
+      
+      # Section 8: Exploring Autonomous Systems
+      # ... (include all steps from Section 8)
+    ]
+  }
+}
